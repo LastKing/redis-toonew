b99e42ed51423b6729261871aaad2ca6c1d9d3a3
litaixin01 <litaixin01@163.com>
2018-01-03 14:23:43 +0800

    修改一个 现实错误，老是重复刷新的问题

--- admin/module/adsModule/adsCtrl.js
+++ admin/module/adsModule/adsCtrl.js
@@ -12,7 +12,7 @@ let adsCtrl = adsModule.controller('adsCtrl', function ($scope, $http, toaster) 
 
     $http.get(`/redis/fields?key=${key}`).then(function (doc) {
       $scope.keys = doc.data;
-      getHkeyLength($scope.select_key_name);
+      getHkeysLength($scope.select_key_name);
     }).catch(function (err) {
       console.error(err);
     });
@@ -21,14 +21,14 @@ let adsCtrl = adsModule.controller('adsCtrl', function ($scope, $http, toaster) 
 
 
   /**
-   * 保存测试服务器上的数据到本地
+   * 保存服务器上的数据到本地
    */
   $scope.saveToLocal = function (type) {
     $http.get(`/redis/saveToLocal?type=${type}&key=${$scope.select_key_name}`).then(function (doc) {
       if (doc.data.status === 'success') {
         toaster.pop('info', 'save', '保存成功');
 
-        $scope.getAllField();
+        $scope.getAllField($scope.select_key_name);
       }
 
     }).catch(function (err) {
@@ -64,13 +64,12 @@ let adsCtrl = adsModule.controller('adsCtrl', function ($scope, $http, toaster) 
 
 
   //获取当前显示的keys长度
-  function getHkeyLength(key) {
+  function getHkeysLength(key) {
     $http.get(`/redis/hkeyLength?key=${key}`).then(function (doc) {
       $scope.fieldLength = doc.data;
     });
   }
 
-  getHkeyLength($scope.select_key_name);
-
+  getHkeysLength();
 
 });
